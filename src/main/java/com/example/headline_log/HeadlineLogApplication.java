f3975bd658f4595b5237980d26c74556c3f7a000
lminjung99 <lminjung99@konkuk.ac.kr>
2023-10-15 17:08:27 +0900

    commit

---
+++ src/main/java/com/example/headline_log/HeadlineLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.headline_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadlineLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadlineLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/headline_log/HeadlineLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.headline_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadlineLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
